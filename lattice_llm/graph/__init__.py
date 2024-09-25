b35844196766118854e536da65c3d0bd6a27e3df
Josh Carver <jcarver989@gmail.com>
2024-09-24 21:02:25 -0700

    add EdgeDestination as an export from __init__.py

--- lattice_llm/graph/__init__.py
+++ lattice_llm/graph/__init__.py
@@ -1,2 +1,2 @@
-from .graph import Graph, GraphExecutionResult, END, START, Node, NodeOrId
+from .graph import Graph, GraphExecutionResult, END, START, Node, NodeOrId, EdgeDestination
 from .execution import run_graph, run_chatbot_on_cli
